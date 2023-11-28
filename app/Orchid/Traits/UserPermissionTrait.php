68c7f7da76a8fe3afa85fb00a4d7de4b41e09c4c
winex01 <winnie131212592@gmail.com>
2023-11-28 15:03:16 +0800

    only show action button dropdown if there is a list avaialble

--- app/Orchid/Traits/UserPermissionTrait.php
+++ app/Orchid/Traits/UserPermissionTrait.php
@@ -4,6 +4,7 @@ namespace App\Orchid\Traits;
 
 trait UserPermissionTrait
 {
+
     public function canCreate($prefix)
     {
         return auth()->user()->hasAccess($prefix.'.create');
@@ -17,5 +18,19 @@ trait UserPermissionTrait
     public function canDelete($prefix)
     {
         return auth()->user()->hasAccess($prefix.'.delete');
+    }
+
+    public function canAny(string $prefix, array $permissions)
+    {
+        // append dot notation
+        $prefix = $prefix.'.';
+
+        // Append roles to each item in the array
+        $actionsWithRoles = array_map(function ($action) use ($prefix) {
+            return $prefix . $action;
+        }, $permissions);
+
+
+        return auth()->user()->hasAnyAccess($actionsWithRoles);
     }
 }
