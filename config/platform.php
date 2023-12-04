3438dbb63e8bc588a929f5c1292ff4a41f0ef0f7
winex01 <winnie131212592@gmail.com>
2023-12-04 14:24:05 +0800

    add custom css to change pagination active style

--- config/platform.php
+++ config/platform.php
@@ -126,7 +126,9 @@ return [
      */
 
     'resource' => [
-        'stylesheets' => [],
+        'stylesheets' => [
+            '/css/custom.css'
+        ],
         'scripts'     => [
             '/js/custom.js'
         ],
